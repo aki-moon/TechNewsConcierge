a130c58eb56c62386d47db47b31f560e3bf43686
aki matsuno <matsuaki4@gmail.com>
2022-10-22 10:49:14 +0900

    initial commit

---
+++ src/main/java/aki/moon/TechNewsConcierge/TechNewsConciergeApplication.java
@@ -0,0 +1,13 @@
+package aki.moon.TechNewsConcierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsConciergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsConciergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aki/moon/TechNewsConcierge/TechNewsConciergeApplicationTests.java
@@ -0,0 +1,13 @@
+package aki.moon.TechNewsConcierge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsConciergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
