3c89b098334df5bf48985ac0177927266d65d8f2
aki matsuno <matsuaki4@gmail.com>
2022-10-29 12:46:11 +0900

    Jooqを用いてDBに格納されている値を読み取れるようにした

--- src/main/java/aki/moon/TechNewsConcierge/GreetingController.java
+++ src/main/java/aki/moon/TechNewsConcierge/GreetingController.java
@@ -1,22 +1,38 @@
 package aki.moon.TechNewsConcierge;
 
+import aki.moon.TechNewsConcierge.tables.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.jooq.DSLContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Controller
 public class GreetingController {
+    private DSLContext create;
+
+    @Autowired
+    public GreetingController(DSLContext dslContext){
+        this.create = dslContext;
+    }
 
     @GetMapping("/article/{id}")
     public String article(Model model, @PathVariable String id) {
-        Article article = new Article("ID",
-                "2022/7/16(土) 11:00",
-                "ワンキャリアのCTOが登壇するオンラインセミナー「CTOが特別解説！『サービススケールで直面した組織課題とマイクロサービスアーキテクチャ戦略』」が7月21日に開催",
-                "翔泳社とCodeZineが主催する開発者向けイベント「Developers Summit 2022&Summer（デブサミ2022夏）」が7月21日にオンラインで開催されます。今年は「アジャイル・DevOps時代のプロダクトとエンジニア組織を支える、Developer Productivityへの道」をテーマとした34セッションを提供します。今回は「開発プロセス」のカテゴリからいくつかのセッションをご紹介します。",
-                "http://codezine.jp/article/detail/16206");
-        model.addAttribute("article", article);
+        Article article = Article.ARTICLE;
+        List<aki.moon.TechNewsConcierge.Article> selected =create.select(Article.ARTICLE.asterisk()).from(article).fetchInto(aki.moon.TechNewsConcierge.Article.class);
+
+        aki.moon.TechNewsConcierge.Article article2 = selected.stream().map(articleVo -> {
+            aki.moon.TechNewsConcierge.Article article1 = new aki.moon.TechNewsConcierge.Article(articleVo.id(), articleVo.publishDate(), articleVo.title(), articleVo.description(), articleVo.link());
+            return article1;
+        }).collect(Collectors.toList()).get(0);
+
+        model.addAttribute("article", article2);
+
         return "article/article";
     }
 
--- src/main/java/aki/moon/TechNewsConcierge/TechNewsConciergeApplication.java
+++ src/main/java/aki/moon/TechNewsConcierge/TechNewsConciergeApplication.java
@@ -2,6 +2,7 @@ package aki.moon.TechNewsConcierge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
 @SpringBootApplication
 public class TechNewsConciergeApplication {
