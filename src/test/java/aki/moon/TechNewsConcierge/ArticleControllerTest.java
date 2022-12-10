10ebe482d9951bb933f7f5000fcbf6febce0793c
aki matsuno <matsuaki4@gmail.com>
2022-12-10 12:48:14 +0900

    ステータスが正常であることを確認するテストを追加

--- src/test/java/aki/moon/TechNewsConcierge/ArticleControllerTest.java
+++ src/test/java/aki/moon/TechNewsConcierge/ArticleControllerTest.java
@@ -1,24 +1,34 @@
 package aki.moon.TechNewsConcierge;
 
-import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.ui.Model;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
 
-@ExtendWith(SpringExtension.class)
-@JooqTest
+@WebMvcTest(ArticleController.class)
 public class ArticleControllerTest {
     @Autowired
-    private ArticleController articleController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ArticleRepository articleRepository;
 
     @Test
-    public void articleTest() throws Exception {
+    public void articleTest1() throws Exception {
+        Article expect = new Article("200",  "2022-12-10", "title", "description", "https://www.example.com/blog/999");
+        when(articleRepository.findById("200")).thenReturn(expect);
+        mockMvc.perform(get("/article/200")).andExpect(status().isOk()).andExpect(content().string(containsString("title")));
     }
 
+    @Test
+    public void articleTest2() throws Exception {
+        mockMvc.perform(get("/article/100")).andExpect(status().isNotFound()).andExpect(content().string(containsString("該当の記事がありませんでした")));
+    }
 }
