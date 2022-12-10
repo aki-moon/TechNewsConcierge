ba4b9716d0711681f6ad6dc88d7dc2fb4735e6ef
aki matsuno <matsuaki4@gmail.com>
2022-12-10 11:11:26 +0900

    Controller層でDBを見ていたのでrepository層にロジックを逃した

---
+++ src/main/java/aki/moon/TechNewsConcierge/ArticleRepository.java
@@ -0,0 +1,21 @@
+package aki.moon.TechNewsConcierge;
+
+import org.jooq.DSLContext;
+import static aki.moon.TechNewsConcierge.tables.Article.ARTICLE;
+
+import java.util.List;
+
+public class ArticleRepository {
+    private DSLContext create;
+    public ArticleRepository(DSLContext dslContext){
+        this.create = dslContext;
+    }
+
+    public Article findById(String id) {
+        List<Article> selected =create.select(ARTICLE.asterisk())
+                .from(ARTICLE)
+                .where(ARTICLE.ID.eq(Integer.valueOf(id)))
+                .fetchInto(aki.moon.TechNewsConcierge.Article.class);
+        return selected.stream().map(article -> new Article(article.id(), article.publishDate(), article.title(), article.description(), article.link())).toList().get(0);
+    }
+}
---
+++ src/test/java/aki/moon/TechNewsConcierge/ArticleRepositoryTest.java
@@ -0,0 +1,36 @@
+package aki.moon.TechNewsConcierge;
+
+import aki.moon.TechNewsConcierge.tables.records.ArticleRecord;
+import org.jooq.DSLContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
+
+import java.time.LocalDate;
+
+import static aki.moon.TechNewsConcierge.tables.Article.ARTICLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JooqTest
+public class ArticleRepositoryTest {
+    @Autowired
+    private DSLContext dslContext;
+
+    @Test
+    public void t1() {
+        Object expect = new Article("200",  "2022-12-10", "title", "description", "https://www.example.com/blog/999");
+        String id = "200";
+
+        ArticleRecord articleRecord = this.dslContext.newRecord(ARTICLE);
+        articleRecord.setId(200);
+        articleRecord.setTitle("title");
+        articleRecord.setDescription("description");
+        articleRecord.setLink("https://www.example.com/blog/999");
+        articleRecord.setPublishdate(LocalDate.of(2022, 12, 10));
+        articleRecord.store();
+
+        ArticleRepository repository = new ArticleRepository(dslContext);
+        Object actual = repository.findById(id);
+        assertEquals(expect, actual);
+    }
+}
