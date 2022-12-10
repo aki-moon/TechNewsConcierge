ac9f73d85c80ab2676a5ee941212a9c72fcd5bf0
aki matsuno <matsuaki4@gmail.com>
2022-12-10 11:36:19 +0900

    ControllerでDBにアクセスしないようにした

--- src/main/java/aki/moon/TechNewsConcierge/ArticleController.java
+++ src/main/java/aki/moon/TechNewsConcierge/ArticleController.java
@@ -1,16 +1,11 @@
 package aki.moon.TechNewsConcierge;
 
-import aki.moon.TechNewsConcierge.tables.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.jooq.DSLContext;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 @Controller
 public class ArticleController {
@@ -23,15 +18,10 @@ public class ArticleController {
 
     @GetMapping("/article/{id}")
     public String article(Model model, @PathVariable String id) {
-        Article article = Article.ARTICLE;
-        List<aki.moon.TechNewsConcierge.Article> selected =create.select(Article.ARTICLE.asterisk()).from(article).fetchInto(aki.moon.TechNewsConcierge.Article.class);
+        ArticleRepository articleRepository = new ArticleRepository(create);
+        Article article = articleRepository.findById(id);
 
-        aki.moon.TechNewsConcierge.Article article2 = selected.stream().map(articleVo -> {
-            aki.moon.TechNewsConcierge.Article article1 = new aki.moon.TechNewsConcierge.Article(articleVo.id(), articleVo.publishDate(), articleVo.title(), articleVo.description(), articleVo.link());
-            return article1;
-        }).collect(Collectors.toList()).get(0);
-
-        model.addAttribute("article", article2);
+        model.addAttribute("article", article);
 
         return "article/article";
     }
